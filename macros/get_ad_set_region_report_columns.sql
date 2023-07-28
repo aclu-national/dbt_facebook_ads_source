9dfb7a19d59663181ee410a25738bbcbcbada69c
Lindsay Arvin <98600929+lindsayarvin@users.noreply.github.com>
2023-07-28 09:05:14 -0700

    Revert "Updates from upstream package"

--- macros/get_ad_set_region_report_columns.sql
+++ macros/get_ad_set_region_report_columns.sql
@@ -1,13 +1,13 @@
 {% macro get_ad_set_region_report_columns() %}
 
 {% set columns = [
-    {"name": "campaign_name", "datatype": dbt.type_string()},
-    {"name": "ad_set_name", "datatype": dbt.type_string()},
-    {"name": "region", "datatype": dbt.type_string()},
-    {"name": "attribution_setting", "datatype": dbt.type_string()},
+    {"name": "campaign_name", "datatype": dbt_utils.type_string()},
+    {"name": "ad_set_name", "datatype": dbt_utils.type_string()},
+    {"name": "region", "datatype": dbt_utils.type_string()},
+    {"name": "attribution_setting", "datatype": dbt_utils.type_string()},
     {"name": "day", "datatype": "date"},
-    {"name": "impressions", "datatype": dbt.type_int()},
-    {"name": "amount_spent_usd", "datatype": dbt.type_float()},
+    {"name": "impressions", "datatype": dbt_utils.type_int()},
+    {"name": "amount_spent_usd", "datatype": dbt_utils.type_float()},
     {"name": "starts", "datatype": "date"},
     {"name": "ends", "datatype": "date"},
 ] %}
--- macros/get_ad_set_report_columns.sql
+++ macros/get_ad_set_report_columns.sql
@@ -1,16 +1,16 @@
 {% macro get_ad_set_report_columns() %}
 
 {% set columns = [
-    {"name": "account_id", "datatype": dbt.type_string()},
-    {"name": "account_name", "datatype": dbt.type_string()},
-    {"name": "campaign_id", "datatype": dbt.type_string()},
-    {"name": "campaign_name", "datatype": dbt.type_string()},
-    {"name": "ad_set_id", "datatype": dbt.type_string()},
-    {"name": "ad_set_name", "datatype": dbt.type_string()},
+    {"name": "account_id", "datatype": dbt_utils.type_string()},
+    {"name": "account_name", "datatype": dbt_utils.type_string()},
+    {"name": "campaign_id", "datatype": dbt_utils.type_string()},
+    {"name": "campaign_name", "datatype": dbt_utils.type_string()},
+    {"name": "ad_set_id", "datatype": dbt_utils.type_string()},
+    {"name": "ad_set_name", "datatype": dbt_utils.type_string()},
     {"name": "day", "datatype": "date"},
-    {"name": "impressions", "datatype": dbt.type_int()},
-    {"name": "link_clicks", "datatype": dbt.type_int()},
-    {"name": "amount_spent_usd", "datatype": dbt.type_float()}
+    {"name": "impressions", "datatype": dbt_utils.type_int()},
+    {"name": "link_clicks", "datatype": dbt_utils.type_int()},
+    {"name": "amount_spent_usd", "datatype": dbt_utils.type_float()}
 ] %}
 
 {{ fivetran_utils.add_pass_through_columns(columns, var('facebook_ads__basic_ad_passthrough_metrics')) }}
